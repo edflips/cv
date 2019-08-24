e529849fea845f8ef1710469ebaeb5cac2dbc7cc
Ed Phillips <ed@paperuncle.com>
2019-08-24 01:15:23 +0100

    refactor card template

--- components/Card.vue
+++ components/Card.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="card">
-    <h2>
-      <font-awesome-icon class="icon" icon="star-of-life" pull="left" />
+  <div class="card mt-2 mb-4 pt-2">
+    <h2 class="flex flex-wrap items-baseline justify-left mt-2">
+      <font-awesome-icon
+        class="icon"
+        :icon="['fas', 'star-of-life']"
+        pull="left"
+      />
       {{ title }}
     </h2>
-    <p>{{ dateRange }}</p>
     <p>{{ description }}</p>
+    <ul class="flex flex-wrap items-baseline justify-left">
+      <li v-for="t in getIcons" :key="t.name">
+        <div
+          v-if="t.icon"
+          class="tech flex flex-no-wrap items-center justify-left pt-1 pb-2 px-2 mr-1 rounded text-pubrown"
+        >
+          <!-- eslint-disable-next-line vue/no-v-html -->
+          <span class="tech-icon" v-html="t.icon" />
+          <span class="tech-title opacity-0 text-xs ml-2">{{ t.name }}</span>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Icons from '~/plugins/tech-icons'
+
 export default {
   name: 'Card',
   props: {
@@ -17,32 +34,45 @@ export default {
       type: String,
       required: true
     },
-    dateRange: {
-      type: String,
-      required: true
-    },
     description: {
       type: String,
       required: true
+    },
+    tech: {
+      type: Array,
+      default: () => [],
+      required: false
     }
-  }
+  },
+  data() {
+    return {
+      hoverShow: false
+    }
+  },
+  computed: {
+    getIcons(props) {
+      return Icons(props.tech)
+    }
+  },
+  methods: {}
 }
 </script>
 
-<style scoped>
-/* .card {
-  text-align: left;
-  padding: 1rem;
+<style>
+.tech-icon svg {
+  width: 1.2rem;
+  fill: theme('colors.pubrown');
 }
-h2 {
-  display: flex;
-  align-items: center;
-  line-height: 1;
-  margin: 1rem 0 0.4rem;
+.card {
+  border-top: 1px dotted;
+  @apply border-gray-400;
 }
-.icon {
-  font-size: 1rem;
-  margin: 0 0.4rem;
-  color: #ff5545;
-} */
+ul .tech {
+  z-index: 1;
+  position: relative;
+}
+
+.tech span.tech-title {
+  @apply opacity-100;
+}
 </style>
